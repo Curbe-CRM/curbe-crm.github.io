9fbf9096dd503fc29549f02c560f0bdb4ef88a39
EstebanBacuilima <davidbacuilima13@gmail.com>
2024-04-07 14:52:24 -0500

    fix: update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,12 +3,11 @@ importScripts("https://www.gstatic.com/firebasejs/10.4.0/firebase-messaging-comp
 
 firebase.initializeApp({
   apiKey: "AIzaSyD1B7kx2tZTsOtaFtyyLdPm2nSC1rD8B4k",
-  appId: "1:925070488780:web:483bdd883edafe1695be69",
-  messagingSenderId: "925070488780",
-  projectId: "karnataka-e1e23",
   authDomain: "karnataka-e1e23.firebaseapp.com",
-  databaseURL: "xxxxxxxxxxxxxxxxxxx",
+  projectId: "karnataka-e1e23",
   storageBucket: "karnataka-e1e23.appspot.com",
+  messagingSenderId: "925070488780",
+  appId: "1:925070488780:web:483bdd883edafe1695be69",
   measurementId: "G-0PZ7TKPEKX",
 });
 
