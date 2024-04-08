f1b8c986c06165d46dd6444b207b4d98e5423489
EstebanBacuilima <davidbacuilima13@gmail.com>
2024-04-08 14:54:08 -0500

    fix: update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,11 +14,27 @@ firebase.initializeApp({
 // Necessary to receive background messages:
 const messaging = firebase.messaging();
 
+let darkPatternsValue = 0;
+
+self.addEventListener("message", function (event) {
+    if (event.data && event.data.type === "SET_DARK_PATTERNS_VALUE") {
+        darkPatternsValue = event.data.value;
+    }
+});
+
 messaging.onBackgroundMessage(function(payload) {
-  console.log('Received background message ', payload);
-  const notificationTitle = payload.data['title'];
+  if (darkPatternsValue != 1) {
+      return;
+  }
+  lastNotificationId = currentNotificationId;
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: payload.data['body'],
+      body: payload.notification.body,
   };
-  self.registration.showNotification(notificationTitle,notificationOptions);
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener("notificationclick", function (event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow("/?source=notification"));
 });
