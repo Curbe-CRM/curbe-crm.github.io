c5487a4deeec40c9ed67f2c0dd8c4f49b12a3afb
EstebanBacuilima <davidbacuilima13@gmail.com>
2024-09-03 09:14:36 -0500

    fix: build

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,47 +1,56 @@
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.4.0/firebase-app-compat.js"
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.4.0/firebase-messaging-compat.js"
-);
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
-firebase.initializeApp({
-  apiKey: "AIzaSyD1B7kx2tZTsOtaFtyyLdPm2nSC1rD8B4k",
-  authDomain: "karnataka-e1e23.firebaseapp.com",
-  projectId: "karnataka-e1e23",
-  storageBucket: "karnataka-e1e23.appspot.com",
-  messagingSenderId: "925070488780",
-  appId: "1:925070488780:web:483bdd883edafe1695be69",
-  measurementId: "G-0PZ7TKPEKX",
-});
+firebaseConfig = {
+    apiKey: "AIzaSyD1B7kx2tZTsOtaFtyyLdPm2nSC1rD8B4k",
+    appId: "1:925070488780:web:483bdd883edafe1695be69",
+    messagingSenderId: "925070488780",
+    projectId: "karnataka-e1e23",
+    authDomain: "karnataka-e1e23.firebaseapp.com",
+    databaseURL: "xxxxxxxxxxxxxxxxxxx",
+    storageBucket: "karnataka-e1e23.appspot.com",
+    measurementId: "G-0PZ7TKPEKX",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 // Necessary to receive background messages:
 const messaging = firebase.messaging();
 
+// messaging.onBackgroundMessage(function (payload) {
+//     console.log("Received background message ", payload);
+//     const notificationTitle = payload.data["title"];
+//     const notificationOptions = {
+//         body: payload.data["body"],
+//     };
+//     self.registration.showNotification(notificationTitle, notificationOptions);
+// });
+
 let darkPatternsValue = 0;
 
 self.addEventListener("message", function (event) {
-  if (event.data && event.data.type === "SET_DARK_PATTERNS_VALUE") {
-    darkPatternsValue = event.data.value;
-  }
+    if (event.data && event.data.type === "SET_DARK_PATTERNS_VALUE") {
+        darkPatternsValue = event.data.value;
+    }
 });
 
 messaging.onBackgroundMessage(function (payload) {
-  if (darkPatternsValue != 1) {
-    return;
-  }
-  lastNotificationId = currentNotificationId;
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
-  return self.registration.showNotification(
-    notificationTitle,
-    notificationOptions
-  );
+    console.log("Received background message ", payload);
+    if (darkPatternsValue != 1) {
+        return;
+    }
+    lastNotificationId = currentNotificationId;
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+    };
+
+    return self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
 self.addEventListener("notificationclick", function (event) {
-  event.notification.close();
-  event.waitUntil(clients.openWindow("/?source=notification"));
+    console.log("Notification clicked ", event);
+    event.notification.close();
+    event.waitUntil(clients.openWindow("/?source=notification"));
 });
